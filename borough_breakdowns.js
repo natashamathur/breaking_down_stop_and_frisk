f7e10e2ced80be42896575a99c3fbf7508c63c31
Natasha Mathur <natashamathur@uchicago.edu>
2019-03-10 20:41:10 -0500

    Add files via upload

---
+++ borough_breakdowns.js
@@ -0,0 +1,119 @@
+const borough_stops= [
+    {'borough': 'Brooklyn', 'val': '0.333', 'tt': 33,
+      'offset': 0,'label': 0.14},
+    {'borough':'Queens','val': '0.223', 'tt': 22,
+      'offset': '0.333',  'label': 0.42},
+    {'borough': 'Manhattan','val': '0.2057','tt': 21,
+      'offset':'0.5569',  'label': 0.62},
+    {'borough': "Bronx",  'val': '0.196','tt': 20,
+      'offset':'0.7626',  'label': 0.84},
+    {'borough': "S.I.",'val': '0.0414','tt': 4,
+      'offset':'0.9586',  'label': 0.965}
+  ]
+
+  const borough_pop = [
+    {'borough': 'Brooklyn', 'val': '0.308', 'tt': 31,
+      'offset': 0,'label': 0.12},
+    {'borough':'Queens','val': '0.278', 'tt': 28,
+      'offset': '0.308',  'label': 0.42},
+    {'borough': 'Manhattan','val': '0.192','tt': 19,
+      'offset':'0.587',  'label': 0.64},
+    {'borough': "Bronx",  'val': '0.166','tt': 17,
+      'offset':'0.779',  'label': 0.84},
+    {'borough': "S.I.",'val': '0.055','tt': 6,
+      'offset':'0.945',  'label': 0.958}
+    ]
+
+function boroughBreakout(data) {
+
+  var z = d3.scaleOrdinal()
+    .range(["#C732D5", "#8AC437", "#F65D3A","#4369EB", "#32D59B"]);
+
+  const svg_borough = d3.select(".borough_breakdowns").append("svg")
+    .attr("width", width)
+    .attr("height", height)
+
+  svg_borough.append('g')
+    .attr('transform', `translate(${margin.left}, ${margin.top})`)
+
+  svg_borough.selectAll("rect")
+       .data(data)
+       .enter()
+       .append("rect")
+       .attr("class", "boroughBlocks")
+       .attr("fill", function(d) { return z(d.val); })
+       .attr("width", d => x(Number(d.val)))
+       .attr("height", height)
+       .attr("y",y)
+       .attr('x', d => x(Number(d.offset)))
+       .on("mouseover", function() { tooltip_borough.style("display", null); })
+   .on("mouseout", function() { tooltip_borough.style("display", "none"); })
+   .on("mousemove", function(d) {
+     var xPosition = d3.mouse(this)[0]-5;
+     var yPosition = d3.mouse(this)[1]-60;
+     tooltip_borough.attr("transform", "translate(" + xPosition + "," + yPosition + ")");
+     tooltip_borough.select("text").text(d.tt + "%")
+     tooltip_borough.attr("fill", "white")
+     tooltip_borough.attr("font-family", "Courier")
+     tooltip_borough.attr("font-size", "18px");
+      })
+    .exit().transition()
+     ;
+
+svg_borough.selectAll("text")
+   .data(data)
+   .enter()
+   .append("text")
+   .attr("class", "boroughTexts")
+   .text(function(d) {
+      return d.borough;
+   })
+   .attr('x', d => x(Number(d.label)))
+   .attr("y", label_y)
+   .attr("font-family", "Courier")
+   .attr("font-size", "16px")
+   .attr("fill", "white");
+
+   // Prep the tooltip bits, initial display is hidden
+var tooltip_borough = svg_borough.append("g")
+.attr("class", "tooltip")
+.style("display", "none")
+
+tooltip_borough.append("text")
+  .attr("dy", label_y)
+
+}
+// code for buttons
+d3.select("#boroughByPopulation")
+.on("click", function(d,i) {
+d3.selectAll(".boroughBlocks")
+  .data(borough_pop).transition().duration(1000)
+  .attr("width", d => x(Number(d.val)))
+  .attr('x', d => x(Number(d.offset)))
+d3.selectAll(".boroughTexts")
+  .data(borough_pop).transition().duration(1000)
+  .text(function(d) {
+     return d.borough;
+  })
+  .attr('x', d => x(Number(d.label)))
+  d3.select("#boroughCaption")
+    .text("Brooklyn is the most populous borough, while Staten Island is the least.")
+});
+
+d3.select("#boroughByStops")
+  .on("click", function(d,i) {
+  d3.selectAll(".boroughBlocks")
+    .data(borough_stops).transition().duration(1000)
+    .attr("width", d => x(Number(d.val)))
+    .attr('x', d => x(Number(d.offset)))
+  d3.selectAll(".boroughTexts")
+    .data(borough_stops).transition().duration(1000)
+    .text(function(d) {
+       return d.borough;
+    })
+    .attr('x', d => x(Number(d.label)))
+    d3.select("#boroughCaption")
+      .text("The number of stops made is actually proportional to the relative population of each borough.")
+  });
+
+boroughBreakout(borough_pop);
---
+++ outcomes.js
@@ -0,0 +1,114 @@
+const data_outcomes= [
+
+    {
+      'race': 'Summons',
+      'val': '0.0588',
+      'tt': '6',
+      'offset': 0,
+      'label': 0
+    },
+    {
+      'race':'Arrests',
+      'val': '0.059959',
+      'tt': '6',
+      'offset': '0.0588',
+      'label': 0.062
+    },
+    {
+      'race':'No Action Taken',
+      'val': '0.8812409',
+      'tt': '88',
+      'offset': '0.118',
+      'label': 0.5
+    }
+  ]
+
+  const data_event= [
+
+      {
+        'race': 'Summons',
+        'val': '0.0588',
+        'tt': '6',
+        'offset': 0,
+        'label': 0
+      },
+      {
+        'race':'Arrests',
+        'val': '0.059959',
+        'tt': '6',
+        'offset': '0.0588',
+        'label': 0.062
+      }
+    ]
+
+    const svg_outcomes = d3.select(".outcomes").append("svg")
+      .attr("width", width)
+      .attr("height", height)
+
+    svg_outcomes.append('g')
+      .attr('transform', `translate(${margin.left}, ${margin.top})`)
+
+      var z = d3.scaleOrdinal()
+        .range(["#C732D5", "#8AC437", "#F65D3A","#4369EB", "#32D59B"]);
+
+function outBreakout(data) {
+
+  const join = svg_outcomes.selectAll("rect")
+       .data(data);
+
+       join.enter()
+       .append("rect")
+       .attr("class","outBlocks")
+       .merge(join)
+       .attr("fill", function(d) { return z(d.val); })
+       .attr("width", d => x(Number(d.val)))
+       .attr("height", height)
+       .attr("y",y)
+       .attr('x', d => x(Number(d.offset)))
+       .on("mouseover", function() { tooltip.style("display", null); })
+   .on("mouseout", function() { tooltip.style("display", "none"); })
+   .on("mousemove", function(d) {
+     var xPosition = d3.mouse(this)[0]-5;
+     var yPosition = d3.mouse(this)[1]-60;
+     tooltip.attr("transform", "translate(" + xPosition + "," + yPosition + ")");
+     tooltip.select("text").text(d.tt + "%")
+     tooltip.attr("fill", "white")
+     tooltip.attr("font-family", "Courier")
+     tooltip.attr("font-size", "18px");
+      });
+
+var join_labels = svg_outcomes.selectAll("text")
+   .data(data);
+
+   join_labels.enter()
+   .append("text")
+   .attr("class","outTexts")
+   .merge(join_labels)
+   .text(function(d) {
+      return d.race;
+   })
+   .attr('x', d => x(Number(d.label)))
+   .attr("y", label_y)
+   .attr("font-family", "Courier")
+   .attr("font-size", "16px")
+   .attr("fill", "white");
+
+   // Prep the tooltip bits, initial display is hidden
+var tooltip = svg_outcomes.append("g")
+.attr("class", "tooltip")
+.style("display", "none")
+
+tooltip.append("text")
+  .attr("dy", label_y)
+
+}
+
+  d3.select("#OutcomeMover")
+  .on("click", function(d,i) {
+    outBreakout(data_outcomes)
+    d3.select("#outCaption")
+      .text("In most cases no actions were taken to follow up on the stop and/or frisk.")
+
+  });
+
+  outBreakout(data_outcomes.slice(0,2))
